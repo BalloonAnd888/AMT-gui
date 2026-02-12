51d9a2a6b6e904fd32e0a297c10cb8eaffba0cf9
Andrew Lau <andrewlau2019@gmail.com>
2026-02-11 21:08:10 -0800

    Added label for mel spectrogram, midi and settings, added reset audio button

--- amt.py
+++ amt.py
@@ -1,9 +1,13 @@
 import sys 
 
-from PySide6.QtCore import QSize
+from PySide6.QtCore import Qt, QSize
 from PySide6.QtWidgets import (
     QApplication, 
     QMainWindow, 
+    QLabel,
+    QHBoxLayout,
+    QVBoxLayout,
+    QWidget,
     )
 
 from audio_control import AudioControl
@@ -19,9 +23,52 @@ class AMTMainWindow(QMainWindow):
 
         self.menu = MenuBar(self)
 
+        # Mel spectrogram
+        mel_spectrogram = QLabel("Mel Spectrogram")
+        font = mel_spectrogram.font()
+        font.setPointSize(30)
+        mel_spectrogram.setFont(font)
+        mel_spectrogram.setAlignment(
+            Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter
+        )
+
+        # MIDI 
+        midi = QLabel("MIDI")
+        font = midi.font()
+        font.setPointSize(30)
+        midi.setFont(font)
+        midi.setAlignment(
+            Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter
+        )
+
         # Audio buttons
         self.audio_control = AudioControl()
-        self.setCentralWidget(self.audio_control.audio_buttons)
+
+        # Settings
+        settings = QLabel("Settings")
+        font = settings.font()
+        font.setPointSize(30)
+        settings.setFont(font)
+        settings.setAlignment(
+            Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter
+        )
+
+        audioLayout = QVBoxLayout()
+        audioLayout.addWidget(self.audio_control.audio_buttons)
+
+        bottom = QHBoxLayout()
+        bottom.addLayout(audioLayout)
+        bottom.addWidget(settings)
+
+        layout = QVBoxLayout()
+        layout.addWidget(mel_spectrogram)
+        layout.addWidget(midi)
+
+        layout.addLayout(bottom)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
 
 app = QApplication(sys.argv)
 
--- audio_control.py
+++ audio_control.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import (
+    QMessageBox,
     QPushButton, 
     QWidget,
     QVBoxLayout,
@@ -10,20 +11,39 @@ class AudioControl():
 
         # Audio buttons
         self.load_audio_button = QPushButton("Load Audio")
-        self.load_audio_button.clicked.connect(self.load_audio)
+        self.load_audio_button.clicked.connect(self.load_audio_button_clicked)
 
         self.record_audio_button = QPushButton("Record Audio")
-        self.record_audio_button.clicked.connect(self.record_audio)
+        self.record_audio_button.clicked.connect(self.record_audio_button_clicked)
+
+        self.reset_audio_button = QPushButton("Reset Audio")
+        self.reset_audio_button.clicked.connect(self.reset_audio_button_clicked)
 
         self.audio_buttons_layout = QVBoxLayout()
         self.audio_buttons_layout.addWidget(self.load_audio_button)
         self.audio_buttons_layout.addWidget(self.record_audio_button)
+        self.audio_buttons_layout.addWidget(self.reset_audio_button)
 
         self.audio_buttons = QWidget()
         self.audio_buttons.setLayout(self.audio_buttons_layout)
 
-    def load_audio(self):
+    def load_audio_button_clicked(self):
         print("Load Audio button clicked")
         
-    def record_audio(self):
+    def record_audio_button_clicked(self):
         print("Record Audio button clicked")
+    
+    def reset_audio_button_clicked(self):
+        print("Reset Audio button clicked")
+        
+        button = QMessageBox.question(
+            self.audio_buttons,
+            "Reset Audio",
+            "Are you sure you want to reset the audio?",
+            QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
+        )
+
+        if button == QMessageBox.StandardButton.Yes:
+            print("Audio reset")
+        else:
+            print("Audio not reset")
